7ab64e82362c74a445fe4e30a9ccd6e70152f744
Felix Rindt <felix@rindt.me>
2023-11-07 21:44:58 +0100

    add translation

--- ephios_youthwarden/apps.py
+++ ephios_youthwarden/apps.py
@@ -1,13 +1,13 @@
 from ephios.core.plugins import PluginConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class PluginApp(PluginConfig):
     name = "ephios_youthwarden"
 
     class EphiosPluginMeta:
-        name = "ephios_youthwarden"
+        name = _("Youth warden flow")
         author = "Julian B. <julian@ephios.de>"
-        description = "This plugin lets youth wardens decide about participations of minors"
+        description = _("This plugin lets youth wardens decide about participations of minors.")
 
     def ready(self):
         from . import signals  # NOQA
